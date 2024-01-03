c47432fb8584f3bee22aa510c4937f0c279ff866
Ait Slimane <rachid.a81@yahoo.com>
2024-01-03 03:40:06 +0100

    Je test plusieurs heuristique pour calculer le score , ya un problem dans le scraping to graphe avec les champion JG

--- Heuristique.py
+++ Heuristique.py
@@ -4,14 +4,44 @@ import matplotlib.pyplot as plt
 graphe = nx.read_graphml("./mon_graphe.graphml") # le graphe contien les données champion + lien
 
 
-def inspecter_attributs_liens(graphe, champion):
-    # Afficher les attributs des liens pour le champion spécifié
-    for voisin in graphe.neighbors(champion):
-        attributs_lien = graphe[champion][voisin]
-        print(f"Attributs du lien entre {champion} et {voisin}: {attributs_lien}")
+def calculer_moyenne_ponderee(graphe, champion):
+    types_liens = ['Counter', 'Synergy', 'Matchup']
+    moyennes_ponderees = {}
 
-# Utiliser la fonction pour inspecter les attributs des liens pour un champion donné
-champion = 'Bard'  # Remplacez par le champion souhaité
-inspecter_attributs_liens(graphe, champion)
+    for type_lien in types_liens:
+        somme_produit_score_poids = 0
+        somme_poids = 0
 
+        # Parcourir tous les voisins du champion avec le type de lien spécifié
+        for voisin in graphe.neighbors(champion):
+            if 'type' in graphe[champion][voisin] and graphe[champion][voisin]['type'] == type_lien:
+                poids_lien = float(graphe[champion][voisin]['label'])
+                score_voisin = float(graphe.nodes[voisin]['Score'])
+                
+                somme_produit_score_poids += score_voisin + abs(poids_lien)
+                somme_poids += 1
 
+        # Calculer la moyenne pondérée pour ce type de lien
+        if somme_poids != 0:
+            moyenne_ponderee_lien = somme_produit_score_poids / somme_poids
+            moyennes_ponderees[type_lien] = moyenne_ponderee_lien
+
+    return moyennes_ponderees
+
+# Utiliser la fonction pour calculer les moyennes pondérées des scores pour un champion donné
+graphe.remove_node('Hwei')
+champion = 'Varus'  # Remplacez par le champion souhaité
+moyennes_ponderees = calculer_moyenne_ponderee(graphe, champion)
+
+# Afficher les moyennes pondérées des scores
+print(f"Moyennes pondérées des scores pour {champion}:")
+for type_lien, moyenne_ponderee in moyennes_ponderees.items():
+    print(f"{type_lien}: {moyenne_ponderee}")
+print(moyennes_ponderees['Counter'])
+norme_counter = moyennes_ponderees['Counter']/100
+norme_matchup = moyennes_ponderees['Matchup']/100
+norme_synergy = moyennes_ponderees['Synergy']/100
+data = graphe.nodes['Varus']
+score_champ = float(data.get('Score',0))/100
+score = 0.2*(norme_matchup-norme_counter) + 0.4*norme_synergy + 0.2*score_champ
+print(score)
